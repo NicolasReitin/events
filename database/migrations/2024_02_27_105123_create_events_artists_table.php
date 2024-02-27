1a9cb92131492f11b1913b673e9c6e8ee1c27441
slimpeas <reitin.pro@gmail.com>
2024-02-27 12:24:38 +0100

    first push

---
+++ database/migrations/2024_02_27_105123_create_events_artists_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events_artists', function (Blueprint $table) {
+            $table->foreignId(column: 'event_id')->constrained(table: 'events');
+            $table->foreignId(column: 'artist_id')->constrained(table: 'artists');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events_artists');
+    }
+};
